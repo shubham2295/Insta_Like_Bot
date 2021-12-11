2a936ea6625877b85c0767920266d171dbfb8478
Shubham Patel <85727423+shubham2295@users.noreply.github.com>
2021-12-11 05:52:20 -0700

    Update InstaLikeBot.java

--- InstaLikeBot.java
+++ InstaLikeBot.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -16,7 +15,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
  */
 public class InstaLikeBot {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 
 		// Taking required data from user		
 		Scanner input = new Scanner(System.in);
